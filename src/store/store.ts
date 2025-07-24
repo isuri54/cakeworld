47f42f4ecdbf86938f24700af76e033b559aecec
isuri54 <154318794+isuri54@users.noreply.github.com>
2025-07-24 18:10:06 +0530

    updated files

--- src/store/store.ts
+++ src/store/store.ts
@@ -11,9 +11,75 @@ export const useStore = create (
             CakeList: CakeData,
             SliceList: SliceData,
             CartPrice: 0,
-            FavouritesList: [],
+            FavoritesList: [],
             CartList: [],
             OrderHistoryList: [],
+            addToFavoriteList: (type: string, id: string) =>
+                set(
+                produce(state => {
+                    if (type == 'Cake') {
+                    for (let i = 0; i < state.CakeList.length; i++) {
+                        if (state.CakeList[i].id == id) {
+                        if (state.CakeList[i].favourite == false) {
+                            state.CakeList[i].favourite = true;
+                            state.FavoritesList.unshift(state.CakeList[i]);
+                        } else {
+                            state.CakeList[i].favourite = false;
+                        }
+                        break;
+                        }
+                    }
+                    } else if (type == 'Slice') {
+                    for (let i = 0; i < state.SliceList.length; i++) {
+                        if (state.SliceList[i].id == id) {
+                        if (state.SliceList[i].favourite == false) {
+                            state.SliceList[i].favourite = true;
+                            state.FavoritesList.unshift(state.SliceList[i]);
+                        } else {
+                            state.SliceList[i].favourite = false;
+                        }
+                        break;
+                        }
+                    }
+                    }
+                }),
+                ),
+            deleteFromFavoriteList: (type: string, id: string) =>
+                set(
+                produce(state => {
+                    if (type == 'Cake') {
+                    for (let i = 0; i < state.CakeList.length; i++) {
+                        if (state.CakeList[i].id == id) {
+                        if (state.CakeList[i].favourite == true) {
+                            state.CakeList[i].favourite = false;
+                        } else {
+                            state.CakeList[i].favourite = true;
+                        }
+                        break;
+                        }
+                    }
+                    } else if (type == 'Slices') {
+                    for (let i = 0; i < state.SliceList.length; i++) {
+                        if (state.SliceList[i].id == id) {
+                        if (state.SliceList[i].favourite == true) {
+                            state.SliceList[i].favourite = false;
+                        } else {
+                            state.SliceList[i].favourite = true;
+                        }
+                        break;
+                        }
+                    }
+                    }
+                    let spliceIndex = -1;
+                    for (let i = 0; i < state.FavoritesList.length; i++) {
+                    if (state.FavoritesList[i].id == id) {
+                        spliceIndex = i;
+                        break;
+                    }
+                    }
+                    state.FavoritesList.splice(spliceIndex, 1);
+                }),
+            ),
         }),
         {
             name: 'cake-app',
